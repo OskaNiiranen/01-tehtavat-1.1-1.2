36e81845756315b9368615c7296c700093ccfb68
OskaNiiranen <oska.niiranen@gmail.com>
2023-09-21 14:16:09 +0300

    Tehtävät 1.1-1.2

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
